eb00a4c1c32121d6bec38adc82d4120670925ca8
gjTool <hwj95@qq.com>
2018-03-09 17:14:28 +0800

    更新饿了么爬数据

--- nodeEleme/js/index.js
+++ nodeEleme/js/index.js
@@ -26,7 +26,7 @@ var show = 'show',hide = 'hide';//要增加的class隐藏或显示
 function pullfreshup() {
 	var _this = this;
 	$.ajax({
-		url: "http://192.168.55.6:3000/",//请求数据的地址
+		url: "http://localhost:3000/",//请求数据的地址
 		type: "GET",
 		success: function(data) {
 			data = JSON.parse(data);//ajax请求过来的是JSON字符串,需要转化为JSON格式(数组Array)
@@ -89,6 +89,68 @@ function pullfreshup() {
 			
 			_this.endPullupToRefresh(false);//数据库数据还没有被全部加载完,可以继续上拉加载
 
+		},
+		error: function(){
+			$.getJSON("js/data.json",function(data){
+				// data = JSON.parse(data);//ajax请求过来的是JSON字符串,需要转化为JSON格式(数组Array)
+				var li = '',len = 6;//li字符串拼接的li元素,len一次刷新加载的数据总条数
+
+				for(var i = 0; i < len; i++) {//每次刷新,循环结束后可以获取的数据条数为len
+
+					li = `
+						<li class="mui-table-view-cell mui-media">
+		        	        <a href="javascript:;">
+		        	        	<div class="imgcon">
+		        	        		<div class="img">
+		        	            		<img class="mui-media-object mui-pull-left" src="http:${data[total].image_path}">
+		        	        		</div>
+		        	        		<div class="xindian ${data[total].is_new}">
+		        	        			<span>新店</span>
+		        	        		</div>
+		        	        	</div>
+		        	            <div class="mui-media-body">
+		        	                <h4 class="${data[total].is_premium}">${data[total].name}</h4>
+		        	                <div class="middle mui-clearfix">
+		        	                	<span class="jRate${total}"></span>
+		        	                	<span class="pinfen">${data[total].rating}</span>
+		        	                	<span class="yueshou">月售${data[total].recent_order_num}单</span>
+		        	                </div>
+		        	                <div class="bottom mui-clearfix">
+		        	                	<span class="qisong">¥${data[total].float_minimum_order_amount}起送</span>
+		        	                	<span class="xian">/</span>
+		        	                	<span class="peisong">配送费¥${data[total].float_delivery_fee}</span>
+		        	                	<div class="juli">
+			        	                	<span class="mi ${parseInt(data[total].distance) > 0 ? show : hide}">${data[total].distance}</span>
+			        	                	<span class="xian ${data[total].order_lead_time > 0 && parseInt(data[total].distance) > 0 ? show : hide}">/</span>
+			        	                	<span class="fenzhong ${data[total].order_lead_time>0?show:hide}">${data[total].order_lead_time}分钟</span>
+		        	                	</div>
+		        	                </div>
+		        	            </div>
+		        	        </a>
+		        	    </li>
+						`;
+					$('#ul').append(li);//每次循环将li加入到ul
+					//五角星评分
+					$(".jRate" + total).jRate({
+						rating: data[total].rating,//评分
+						startColor: "#ffaa0c",//开始的填充颜色
+						endColor: "#ffaa0c",//结束的填充颜色
+						width: 12,
+						height: 12,
+						backgroundColor: 'lightgray'//没有填充的背景底色
+					});
+					//判断上拉加载获取数据库的数据结束,已经把数据库数据都加载完毕
+					if(total >= data.length - 1) {//已经获取的数据总条数大于等于数据库的总条数时
+						_this.endPullupToRefresh(true);//显示没有更多数据
+						setTimeout(function() {
+							mui('#refreshContainer').pullRefresh().disablePullupToRefresh();
+						}, 2000);//2s后执行禁止上拉加载
+						return;//数据库数据都已加载完毕,中断循环
+					}
+					total++;//数据总条数每次加1
+				};
+				_this.endPullupToRefresh(false);//数据库数据还没有被全部加载完,可以继续上拉加载
+			})
 		}
 	});
 
@@ -97,7 +159,7 @@ function pullfreshup() {
 //下拉刷新获取数据的调用方法
 function pullfreshdown() {
 	$.ajax({
-		url: "http://192.168.55.6:3000/",
+		url: "http://localhost:3000/",
 		type: "GET",
 		success: function(data) {
 			data = JSON.parse(data);
